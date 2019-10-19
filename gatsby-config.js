7942e73d6a3fda40302f46491b23628e2c32543a
Jakub Bazgier <kuba@flyhigh.pro>
2019-10-19 14:47:56 +0200

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+module.exports = {
+    siteMetadata: {
+        title: `Gatsby Default Starter`,
+        description: `Awesome custom starter kit`,
+        author: `@tysian`,
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-root-import',
+            options: {
+                assets: path.join(__dirname, 'src/assets'),
+                components: path.join(__dirname, 'src/components'),
+                data: path.join(__dirname, 'data/'),
+                partials: path.join(__dirname, 'src/components/partials'),
+                static: path.join(__dirname, 'static/'),
+                theme: path.join(__dirname, 'src/components/theme'),
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-eslint',
+            options: {
+                test: /\.js$|\.jsx$/,
+                exclude: /(node_modules|.cache|public)/,
+                stages: ['develop'],
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                },
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-stylelint',
+            options: { files: ['**/*.{js,jsx}'] },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};
